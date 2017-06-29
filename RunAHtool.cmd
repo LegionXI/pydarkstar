7f03bb0ff0c5c2ce9adc72c863f2cb4efb5f318f
TeoTwawki <arawn.annwn@yandex.com>
2017-06-29 15:38:35 -0400

    Fix batch path

--- RunAHtool.cmd
+++ RunAHtool.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 :onCrash
 echo [%date% %time%] Starting AH Tool...
-cd "C:\pydarkstar"
-python C:\pydarkstar\broker.py --verbose items.csv
+cd "C:\LegionDark\AH_Bot_old"
+C:\python\python2 C:\LegionDark\AH_Bot_old\broker.py --verbose items.csv
 @ECHO AH-Bot stopped or crashed!
 @ECHO %date% %time%> .\Last_AH-tool_Stop.log
 @ECHO ...
